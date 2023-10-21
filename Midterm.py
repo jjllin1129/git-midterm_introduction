77a03a082bce6f64fef2d1bb526aab18568f6757
DESKTOP-18588J2\USER <skes09605@gmail.com>
2023-10-22 02:13:01 +0800

    更改hyperparameter 把nan值變成0 plot important feature

--- Midterm.py
+++ Midterm.py
@@ -1,40 +1,33 @@
 import pandas as pd
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
 from xgboost import XGBClassifier
-
-#%% 匯入資料
+import matplotlib.pyplot as plt
+#%% create XGBoost model
+my_xgboost = XGBClassifier(learning_rate=0.1, max_depth=5, n_estimators=5000, eval_metric='logloss')
+#%% import data
 data_train = pd.read_csv('train.csv')
-data_xtest = pd.read_csv('X_test.csv')
-
-#%% 分出X Y（training data）
+x_test = pd.read_csv('X_test.csv')
+#%% split x and y (training data）
 x_train = data_train.iloc[:, :-1]
 y_train = data_train.iloc[:, -1]
-
-#%%把NAN變成0
-#列出你想要排除的columns name
-columns_to_exclude = ['D_sms', 'D_sm1', 'D_tm0', 'D_Ra_Capacity', 'D_Ra_CDR']
-# 要替換的columns
-columns_to_fill = [col for col in x_train.columns if col not in columns_to_exclude]
-#  NaN 替换为 0
-x_train[columns_to_fill] = x_train[columns_to_fill].fillna(0)
-
-#%% 對於D_windows進行處理 讓他在0~1之間
-x_train['D_windows'] = x_train['D_windows'].apply(lambda x: min(1, max(0, x)))
-#%% cross validation
-num_trees = 10
-kfold = KFold(n_splits=10)
-model = XGBClassifier(n_estimators=num_trees)
-results = cross_val_score(model, x_train, y_train, cv=kfold)
-print(results.mean())
-
-#%% train XGBoost
-xgbc = XGBClassifier()
-xgbc.fit(x_train, y_train)
-
-#%% predict 
-ypred = xgbc.predict(data_xtest)
-ypred = pd.DataFrame(ypred, columns=['label'])
+#%%train
+my_xgboost.fit(x_train, y_train)
+#%% predict
+y_pred = my_xgboost.predict(x_test)
+#%% nan to  zero
+x_train = x_train.fillna(0)
+#%% export submit
+ypred = pd.DataFrame(y_pred, columns=['label'])
 ypred['id'] = range(1, len(ypred) + 1)
 ypred = ypred[['id', 'label']]
 ypred.to_csv('submission.csv', index=False)
+#%% plot important feature
+feature_importance = my_xgboost.feature_importances_
+feature_names = x_train.columns  
+plt.figure(figsize=(10, 6))
+plt.bar(range(len(feature_names)), feature_importance, tick_label=feature_names)
+plt.xticks(rotation=90)
+plt.show()
+#%% combine importand_feature and features name to a DataFrame
+feature_importance_df = pd.DataFrame(feature_importance)
+feature_importance_df['name'] = feature_names
+
